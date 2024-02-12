d576e087baf90458a7b865e2d9810c6fbdbbeb63
Tate Lewis <tatelewis0@gmail.com>
2024-02-11 20:27:09 -0500

    added a dropdown menu on search(incomplete)

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const search=document.getElementById('search')
+const dropdown=document.getElementById('dropdown')
+const selection = document.getElementById('search-category')
 const form = document.getElementById('form')
 const dataContainer = document.getElementById('data-container')
 const api = "https://api.coinranking.com/v2/coins?";
 const apiKey =
   "coinranking5be8a33da639de52f62d8bd9d85177b4a37dfb6dbcc6eb55";
 async function getData(coin) { //Gets coin data based on search inputs
-  let endpoint = 'limit=5'
-  if(coin) {
-    endpoint = `search=${coin}`
+  let endpoint
+  switch(selection.value) {
+    case 'name':
+      endpoint=`search=${coin}`
+      break;
+    case 'UUID':
+      endpoint = `uuids=${coin}`
+      break;
+    case 'symbol': 
+      endpoint=`symbols=${coin}`
+      break;
+    default:
+      endpoint=`limit=5`
   }
   
   let response = await fetch(api+endpoint, {
@@ -43,7 +55,7 @@ async function getData(coin) { //Gets coin data based on search inputs
       newDiv.appendChild(price)
       newDiv.appendChild(volume)
       newDiv.appendChild(newImg)
-      newImg.addEventListener('click', e=> {
+      newDiv.addEventListener('click', e=> {
         let query = `coinName=${coin.name}&coinUUID=${coin.uuid}`
         window.location.href = `pages/chart.html?${query}`
       })
@@ -110,3 +122,72 @@ document.addEventListener('DOMContentLoaded', e => {
   renderData()
   console.log('Basic content loaded')
 })
+
+search.addEventListener('input', async e=>{
+  if(search.value) {
+    const coins = await autoComplete(search.value)
+    showAutoComplete(coins)
+    } else {
+    return
+  }
+  if(!search.value) {
+    dropdown.innerHTML = ''
+  }
+})
+
+async function autoComplete(query) {
+  const res = await fetch(`https://api.coinranking.com/v2/search-suggestions?query=${query}`)
+  const json = await res.json()
+  if(json.data&&json.data.coins) {
+    return json.data.coins
+  } else {
+    return null
+  }
+}
+
+function showAutoComplete(options) {
+  let droption
+  dropdown.innerHTML = ''
+  options.forEach(option=> {
+    switch(selection.value) {
+      case 'Name': 
+       droption = appendElement('p', 'option', option.name, dropdown)
+    droption.addEventListener('click', e => {
+      search.value = option.name
+      try {
+        renderData(option.name)
+      } catch (err) {
+        console.log(`Error rendering autocomplete data: ${err}`)
+      }
+    })
+    break;
+    case 'UUID':
+       droption = appendElement('p', 'option', option.uuid, dropdown)
+    droption.addEventListener('click', e => {
+      search.value = option.uuid
+      try {
+        renderData(option.uuid)
+      } catch (err) {
+        console.log(`Error rendering autocomplete data: ${err}`)
+      }
+    })
+    break;
+    case 'Symbol':
+      let droption = appendElement('p', 'option', option.symbol, dropdown)
+    droption.addEventListener('click', e => {
+      search.value = option.symbol
+      try {
+        renderData(option.symbol)
+      } catch (err) {
+        console.log(`Error rendering autocomplete data: ${err}`)
+      }
+    })
+    }
+    
+})
+}
+document.addEventListener('click', e => {
+  if (!search.contains(e.target) && !dropdown.contains(e.target)) {
+    dropdown.innerHTML = ''; 
+  }
+}) 
